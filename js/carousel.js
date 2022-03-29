e8c837ef9f473bb6a699c8bffe27f93021594f2a
joaofpdmn <jvfpdaemoon1@gmail.com>
2022-03-29 14:23:58 -0300

    first commit

---
+++ js/carousel.js
@@ -0,0 +1,35 @@
+const track = document.querySelector('.carousel_track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel_button--right');
+const prevButton = document.querySelector('.carousel_button--left');
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+console.log(slideWidth);
+
+/*slides[0].style.left = 0;
+slides[n-1].style.left = slideWidth + 'px';*/
+
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + 'px';
+}
+
+slides.forEach(setSlidePosition);
+
+const moveToSlide = (track, currentSlide, targetSlide) =>{
+    track.style.transform = `translateX(-${targetSlide.style.left})`;
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
+prevButton.addEventListener('click', e =>{
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+    moveToSlide(track, currentSlide, prevSlide);
+})
+
+nextButton.addEventListener('click', e => {
+    const currentSlide =  track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    moveToSlide(track, currentSlide, nextSlide);
+})
